8a7fbb740cb76f7ea86cdedcaea45d4a61e26184
Aaron Zygala <aaronzygala@gmail.com>
2024-08-18 07:12:09 -0400

    Initial definition of Card object

--- src/core/card.cpp
+++ src/core/card.cpp
@@ -1,9 +1,119 @@
-#include <iostream>
 #include "card.hpp"
 
+// Constructor
+Card::Card() : 
+    cost(0), 
+    additionalCost(0), 
+    numberOfCards(0), 
+    modifyCost(false), 
+    inSupply(false), 
+    hasTrashEffect(false), 
+    hasGainEffect(false), 
+    trasher(false), 
+    specialSetup(false), 
+    curseCost(0), 
+    costReducer(false) 
+{}
 
-int main() {
-    std::cout << "Hello World!";
-    Card::Card test();
-    return 0;
+// Destructor
+Card::~Card() {}
+
+// Getter and Setter implementations
+
+std::string Card::getName() const {
+    return name;
+}
+
+void Card::setName(const std::string& name) {
+    this->name = name;
+}
+
+std::string Card::getSet() const {
+    return set;
+}
+
+void Card::setSet(const std::string& set) {
+    this->set = set;
+}
+
+std::vector<std::string> Card::getTypes() const {
+    return types;
+}
+
+void Card::addType(const std::string& type) {
+    types.push_back(type);
+}
+
+int Card::getCost() const {
+    return cost;
+}
+
+void Card::setCost(int cost) {
+    this->cost = cost;
+}
+
+int Card::getAdditionalCost() const {
+    return additionalCost;
+}
+
+void Card::setAdditionalCost(int additionalCost) {
+    this->additionalCost = additionalCost;
+}
+
+int Card::getNumberOfCards() const {
+    return numberOfCards;
+}
+
+void Card::setNumberOfCards(int numberOfCards) {
+    this->numberOfCards = numberOfCards;
+}
+
+bool Card::isModifyCost() const {
+    return modifyCost;
+}
+
+void Card::setModifyCost(bool modifyCost) {
+    this->modifyCost = modifyCost;
+}
+
+bool Card::isInSupply() const {
+    return inSupply;
+}
+
+void Card::setInSupply(bool inSupply) {
+    this->inSupply = inSupply;
+}
+
+bool Card::getHasTrashEffect() const {
+    return hasTrashEffect;
+}
+
+void Card::setHasTrashEffect(bool hasTrashEffect) {
+    this->hasTrashEffect = hasTrashEffect;
+}
+
+bool Card::getHasGainEffect() const {
+    return hasGainEffect;
+}
+
+void Card::setHasGainEffect(bool hasGainEffect) {
+    this->hasGainEffect = hasGainEffect;
+}
+
+// Implement other getters and setters similarly
+
+// Special functions
+void Card::playCard() {
+    // Implementation of card's effect when played
+    // This will vary depending on the specific card
+}
+
+void Card::onTrashEffect() {
+    // Implementation of card's effect when trashed
+    // This will vary depending on the specific card
+}
+
+void Card::onGainEffect() {
+    // Implementation of card's effect when gained
+    // This will vary depending on the specific card
 }
--- src/core/card.hpp
+++ src/core/card.hpp
@@ -1,25 +1,70 @@
-#include <string>
 #ifndef CARD_H
 #define CARD_H
 
-namespace Card{
+#include <string>
+#include <vector>
 
 class Card {
-    public:
-        // Constructors
-        Card();
+public:
+    // Constructor and destructor
+    Card();
+    ~Card();
+    
+    // Attributes based on Dominion cards dataset
+    std::string getName() const;
+    void setName(const std::string& name);
 
-        // Access functions
-        std::string getName(); 
-        int getCost();
-        int getType();
+    std::string getSet() const;
+    void setSet(const std::string& set);
 
-    private:
-        std::string name;
-        int cost;
-        int type;
+    std::vector<std::string> getTypes() const;
+    void addType(const std::string& type);
 
+    int getCost() const;
+    void setCost(int cost);
+
+    int getAdditionalCost() const;
+    void setAdditionalCost(int additionalCost);
+
+    int getNumberOfCards() const;
+    void setNumberOfCards(int numberOfCards);
+
+    bool isModifyCost() const;
+    void setModifyCost(bool modifyCost);
+
+    bool isInSupply() const;
+    void setInSupply(bool inSupply);
+
+    bool getHasTrashEffect() const;
+    void setHasTrashEffect(bool hasTrashEffect);
+
+    bool getHasGainEffect() const;
+    void setHasGainEffect(bool hasGainEffect);
+
+    // Special functions
+    void playCard(); // Implement the card's effect when played
+    void onTrashEffect(); // Implement the card's effect when trashed
+    void onGainEffect(); // Implement the card's effect when gained
+
+private:
+    std::string name;
+    std::string set;
+    std::vector<std::string> types; // Can hold multiple types
+    int cost;
+    int additionalCost;
+    int numberOfCards;
+    bool modifyCost;
+    bool inSupply;
+    bool hasTrashEffect;
+    bool hasGainEffect;
+    bool trasher;
+    bool specialSetup;
+    int curseCost;
+    bool costReducer;
+    std::string associatedToken;
+    std::string associatedToken2;
+    std::string associatedMat;
+    std::vector<std::string> calledCards;
 };
 
-}
-#endif
+#endif // CARD_H
